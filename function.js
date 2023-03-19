7b69aed6c31fd1903316b0361226516afe123215
KatarinaPli <kasiash1010@gmail.com>
2023-03-19 21:42:36 +0100

    created a js file

---
+++ function.js
@@ -0,0 +1,7 @@
+// 1 task
+let now = new Date();
+let currentTime = document.querySelector(".time");
+
+let hour = now.getHours();
+let minute = now.getMinutes();
+time.innerHTML = `${hour}:${minute} PM`;
