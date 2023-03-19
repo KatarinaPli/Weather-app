d01e0794d964b6904ad5ab3f8c0b51696ce3b811
KatarinaPli <kasiash1010@gmail.com>
2023-03-19 22:02:00 +0100

    Added a current Date in js

--- function.js
+++ function.js
@@ -5,3 +5,36 @@ let currentTime = document.querySelector(".time");
 let hour = now.getHours();
 let minute = now.getMinutes();
 currentTime.innerHTML = `${hour}:${minute} <small>PM</small>`;
+
+// 2 task
+let nowTwo = new Date();
+let currentData = document.querySelector(".data");
+
+let currentDayOfWeek = [
+  "Sanday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = currentDayOfWeek[nowTwo.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "Jun",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let currentMonth = months[nowTwo.getMonth()];
+let currentNumber = nowTwo.getDate();
+currentData.innerHTML = `${day}, ${currentNumber} ${currentMonth} `;
