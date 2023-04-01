a94d2276644eb2b31b2af076b3fe64891e026e82
KatarinaPli <kasiash1010@gmail.com>
2023-04-01 12:47:32 +0200

    Added a current temperature in Tokyo in js and current date

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+//current data
 let nowTwo = new Date();
 let currentData = document.querySelector(".data");
 
@@ -29,3 +30,15 @@ let months = [
 let currentMonth = months[nowTwo.getMonth()];
 let currentNumber = nowTwo.getDate();
 currentData.innerHTML = `${day}, ${currentNumber} ${currentMonth} `;
+// call the main temperature
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector(".temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+// api connection
+let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+let apiKey = "4b3503b2f08a729413c4d33ef1186004";
+let units = "metric";
+let apiUrl = `${apiEndpoint}?q=Tokyo&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
