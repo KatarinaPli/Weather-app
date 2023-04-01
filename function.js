bb326ed22124f173eac58d709af94018f8e6147f
KatarinaPli <kasiash1010@gmail.com>
2023-04-01 13:41:29 +0200

    Changed classes in html for time

--- function.js
+++ function.js
@@ -30,6 +30,20 @@ let months = [
 let currentMonth = months[nowTwo.getMonth()];
 let currentNumber = nowTwo.getDate();
 currentData.innerHTML = `${day}, ${currentNumber} ${currentMonth} `;
+
+//Time of a city location with calculates numbers
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${hour}:${minute}`;
+}
 // call the main temperature
 function displayTemperature(response) {
   let temperatureElement = document.querySelector(".temperature");
@@ -37,12 +51,14 @@ function displayTemperature(response) {
   let weatherConditionsElement = document.querySelector(".weather-conditions");
   let humidityElement = document.querySelector(".show-humidity");
   let windElement = document.querySelector(".show-wind");
+  let timeElement = document.querySelector(".time");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   weatherConditionsElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  timeElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 // api connection
 let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
